42c15138ec92a6ab3d7accf4570c1cab57eed384
marcomarcovski <161604330+marcomarcovski@users.noreply.github.com>
2024-11-06 07:53:43 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,6 +4,11 @@ async function visualizarinformacoesGlobais() {
   const res = await fetch (url)
   const dados = await res.json()
   console.log(dados);
+
+
 }
 
 visualizarinformacoesGlobais();
+const paragrafo = document.createelement('p')
+paragrafo.classlist.add('graficos-container_texto')
+paragrafo.innerHTML = `voce sabia que o mundo tem <span> $ {pessoanomundo} bilĥoes/
